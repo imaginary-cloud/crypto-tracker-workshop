09239c86f946b6ea942c82e884366005d3890fa8
Patricia Silva <psilva@imaginarycloud.com>
2022-02-20 18:39:38 +0000

    add base settings of the project + apply prettier changes

---
+++ config-overrides.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+/* config-overrides.js */
+
+module.exports = function override(config, env) {
+  config.module.rules = config.module.rules.map((rule) => {
+    if (rule.oneOf instanceof Array) {
+      return {
+        ...rule,
+        oneOf: [
+          {
+            test: /\.svg$/,
+            use: [
+              {
+                loader: require.resolve('@svgr/webpack'),
+                options: {
+                  prettier: false,
+                  svgo: false,
+                  svgoConfig: {
+                    plugins: [{ removeViewBox: false }],
+                  },
+                  titleProp: true,
+                  ref: true,
+                },
+              },
+              {
+                loader: require.resolve('file-loader'),
+                options: {
+                  name: 'static/media/[name].[hash].[ext]',
+                },
+              },
+            ],
+          },
+          ...rule.oneOf,
+        ],
+      }
+    }
+
+    return rule
+  })
+  return config
+}
+
+/* eslint-enable */
