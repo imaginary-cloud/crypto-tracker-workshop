a563c7cb1e7ac93101d1fb29f4687db77ac4cc65
Patricia Silva <psilva@imaginarycloud.com>
2022-02-20 23:27:33 +0000

    add isLoading state and coinSpinner + see error on network and infinite spinner because of the empty coin

--- src/components/CryptoSearch.jsx
+++ src/components/CryptoSearch.jsx
@@ -2,21 +2,26 @@ import React, { useEffect, useState } from 'react'
 import { useCryptoSearch } from '../hooks/useCryptoSearch'
 
 import SearchInput from './SearchInput'
+import CoinSpinner from './CoinSpinner'
 
 function CryptoSearch() {
   const [coin, setcoin] = useState('')
   const [coinsList, setCoinsList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const getCoin = useCryptoSearch()
 
   useEffect(async () => {
+    setIsLoading(true)
     const result = await getCoin(coin)
+    setIsLoading(false)
     setCoinsList(result)
   }, [coin])
 
   return (
     <>
       <SearchInput setCoin={setcoin} />
+      {isLoading && <CoinSpinner />}
       {coinsList.map((realCoin) => (
         <span>{realCoin.name}</span>
       ))}
