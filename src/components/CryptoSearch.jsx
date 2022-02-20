df263f5704b86f0a549ba2a41d409d657fbfd5a5
Patricia Silva <psilva@imaginarycloud.com>
2022-02-20 23:21:16 +0000

    create state to save coinslist from search + show list under search input

--- src/components/CryptoSearch.jsx
+++ src/components/CryptoSearch.jsx
@@ -5,15 +5,23 @@ import SearchInput from './SearchInput'
 
 function CryptoSearch() {
   const [coin, setcoin] = useState('')
+  const [coinsList, setCoinsList] = useState([])
 
   const getCoin = useCryptoSearch()
 
   useEffect(async () => {
     const result = await getCoin(coin)
-    console.log(result)
+    setCoinsList(result)
   }, [coin])
 
-  return <SearchInput setCoin={setcoin} />
+  return (
+    <>
+      <SearchInput setCoin={setcoin} />
+      {coinsList.map((realCoin) => (
+        <span>{realCoin.name}</span>
+      ))}
+    </>
+  )
 }
 
 export default CryptoSearch
