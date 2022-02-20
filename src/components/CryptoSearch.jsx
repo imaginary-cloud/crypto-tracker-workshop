25a4808762fc1ef4647b848fcfe2ba7e5c507784
Patricia Silva <psilva@imaginarycloud.com>
2022-02-20 23:49:00 +0000

    introducing useQuery - replace function on hook + replace all states and useEffect on CryptoSearch + show error on console asking for QueryClientProvider
    introducing useQuery - replace function on hook + replace all states and useEffect on CryptoSearch + show error on console asking for QueryClientProvider

--- src/components/CryptoSearch.jsx
+++ src/components/CryptoSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useCryptoSearch } from '../hooks/useCryptoSearch'
 
 import SearchInput from './SearchInput'
@@ -6,33 +6,14 @@ import CoinSpinner from './CoinSpinner'
 
 function CryptoSearch() {
   const [coin, setcoin] = useState('')
-  const [coinsList, setCoinsList] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
 
-  const getCoin = useCryptoSearch()
-
-  useEffect(async () => {
-    try {
-      setError(null)
-      setIsLoading(true)
-      const result = await getCoin(coin)
-      setIsLoading(false)
-      setCoinsList(result)
-    } catch (e) {
-      setIsLoading(false)
-      setError('Error during request for: ', coin)
-      setCoinsList([])
-    }
-  }, [coin])
+  const { isLoading, data: coinsList, error } = useCryptoSearch(coin)
 
   return (
     <>
       <SearchInput setCoin={setcoin} />
       {isLoading && <CoinSpinner />}
-      {coinsList.map((realCoin) => (
-        <span>{realCoin.name}</span>
-      ))}
+      {coinsList && coinsList.map((realCoin) => <span>{realCoin.name}</span>)}
       {error && <span>{error}</span>}
     </>
   )
--- src/hooks/useCryptoSearch.js
+++ src/hooks/useCryptoSearch.js
@@ -1,10 +1,15 @@
+import { useQuery } from 'react-query'
+
 import { getSearchCoins } from '../api'
 
-export const useCryptoSearch = () => {
-  const getCoin = async (coin) => {
-    const { data: result } = await getSearchCoins(coin)
-    return result?.currencies || []
-  }
-
-  return getCoin
-}
+export const useCryptoSearch = (coin) =>
+  useQuery(
+    ['crypto-search', coin],
+    async () => {
+      const { data: result } = await getSearchCoins(coin)
+      return result?.currencies || []
+    },
+    {
+      enabled: !!coin,
+    },
+  )
